440259b6ea23528d77d4bfb63f2eb13707755ab0
github-cloudlabsuser-087 <80265464+github-cloudlabsuser-087@users.noreply.github.com>
2021-06-07 18:24:43 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("216476b8-cd8f-4bfa-8c79-3f570a083dda");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
